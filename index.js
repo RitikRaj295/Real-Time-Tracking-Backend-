b46ddfc8a04f22f54be503e388e55f3fbddbd4d7
ritikrathore295@gmail.com <ritikrathore295@gmail.com>
2025-06-17 12:33:48 +0530

    debugging performed

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { Server } from "socket.io";
 const app=express();
 const server=http.createServer(app);
 const io = new Server(server);
+const port=3000;
 
 app.set("view engine","ejs");
 app.use(express.static("./Public"))
@@ -13,10 +14,11 @@ io.on("connection",(socket)=>{
     console.log("new user connected")
     socket.on("send-location",(data)=>{
         io.emit("recieve-location",(data))
-        console.log(socket.id)
     });
 
-    io.emit("user-disconnect",socket.id);
+    socket.on("disconnect",()=>{
+        io.emit("user-disconnect",socket.id)
+    });
 });
 
 
@@ -25,5 +27,5 @@ app.get("/",(req,res)=>{
     res.render("index");
 })
 
-server.listen(3000,()=>{console.log("Server is listening on port no: 3000")});
+server.listen(port,()=>{console.log("Server is listening on port no: 3000")});
 
